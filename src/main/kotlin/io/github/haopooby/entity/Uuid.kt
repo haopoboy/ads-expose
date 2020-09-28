0a530e5d63995f4804cd64a142893b94fd60fed2
haopo <haopoboy@gmail.com>
2020-09-28 16:07:40 +0800

    Implement equals and hashCode for Uuid

--- src/main/kotlin/io/github/haopooby/entity/Uuid.kt
+++ src/main/kotlin/io/github/haopooby/entity/Uuid.kt
@@ -7,4 +7,20 @@ import javax.persistence.MappedSuperclass
 @MappedSuperclass
 open class Uuid(
         @get:Id @get:javax.persistence.Id var id: String = UUID.randomUUID().toString()
-)
+) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Uuid
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+}
