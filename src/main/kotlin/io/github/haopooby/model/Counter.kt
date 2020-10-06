7650c91364ba894e7a2b5f16363a33912f02a952
haopo <haopoboy@gmail.com>
2020-10-06 13:47:09 +0800

    Improve performance by using lesser memory

--- src/main/kotlin/io/github/haopooby/model/Counter.kt
+++ src/main/kotlin/io/github/haopooby/model/Counter.kt
@@ -4,20 +4,19 @@ import com.github.benmanes.caffeine.cache.Cache
 import com.github.benmanes.caffeine.cache.Caffeine
 import io.github.haopooby.entity.Ads
 import org.checkerframework.checker.nullness.qual.NonNull
-import java.util.*
 import java.util.concurrent.TimeUnit
 
 data class Counter(val ads: Ads = Ads(),
-                   val count: @NonNull Cache<String, String> = buildCache(ads)
+                   val count: @NonNull Cache<Long, Unit> = buildCache(ads)
 ) {
     companion object {
-        fun buildCache(ads: Ads): @NonNull Cache<String, String> {
+        fun buildCache(ads: Ads): @NonNull Cache<Long, Unit> {
             return Caffeine.newBuilder()
                     .expireAfterWrite(ads.capIntervalMin.toLong(), TimeUnit.MINUTES)
-                    .build<String, String>()
+                    .build()
         }
     }
 
     fun allowed() = count.estimatedSize() < ads.capNum
-    fun increase() = count.put(UUID.randomUUID().toString(), "")
+    fun increase() = count.put(count.estimatedSize(), Unit)
 }
