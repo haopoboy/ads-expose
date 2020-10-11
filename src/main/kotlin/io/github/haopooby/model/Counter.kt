9e749a23f94d4b8647a42e1d27d24b48ac76da3b
haopo <haopoboy@gmail.com>
2020-10-11 15:33:23 +0800

    Fix issue of count if it's expired

--- src/main/kotlin/io/github/haopooby/model/Counter.kt
+++ src/main/kotlin/io/github/haopooby/model/Counter.kt
@@ -4,13 +4,14 @@ import com.github.benmanes.caffeine.cache.Cache
 import com.github.benmanes.caffeine.cache.Caffeine
 import io.github.haopooby.entity.Ads
 import org.checkerframework.checker.nullness.qual.NonNull
+import java.util.*
 import java.util.concurrent.TimeUnit
 
 data class Counter(val ads: Ads = Ads(),
-                   val count: @NonNull Cache<Long, Unit> = buildCache(ads)
+                   val count: @NonNull Cache<String, Unit> = buildCache(ads)
 ) {
     companion object {
-        fun buildCache(ads: Ads): @NonNull Cache<Long, Unit> {
+        fun buildCache(ads: Ads): @NonNull Cache<String, Unit> {
             return Caffeine.newBuilder()
                     .expireAfterWrite(ads.capIntervalMin.toLong(), TimeUnit.MINUTES)
                     .build()
@@ -18,5 +19,5 @@ data class Counter(val ads: Ads = Ads(),
     }
 
     fun allowed() = count.estimatedSize() < ads.capNum || !ads.exposedLimited
-    fun increase() = count.put(count.estimatedSize(), Unit)
+    fun increase() = count.put(UUID.randomUUID().toString(), Unit)
 }
